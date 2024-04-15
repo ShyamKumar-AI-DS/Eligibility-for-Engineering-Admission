6df67fe755e0c0e40e44e9c748844ecdf6f648c3
Santino_44 <93427182+ShyamKumar-AI-DS@users.noreply.github.com>
2024-04-15 20:29:10 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,38 @@
-﻿using System;
-    class Eligibility
+using System;
+namespace program11
+{
+    class Program
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hiiiiiii");
+            string name;
+            int math, phy, chem;
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+            Console.Write("Enter Maths Marks:");
+            math = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Physics Marks:");
+            phy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Chemistry Marks:");
+            chem = Convert.ToInt32(Console.ReadLine());
+            int Sum = math + phy + chem;
+            int MPTot = math + phy;
+            if (math >= 65 && phy >= 55 && chem >= 50)
+            {
+                if (Sum >= 180 || MPTot >= 140)
+                {
+                    Console.WriteLine(name + " is eligible for admission to an engineering course.");
+                }
+                else
+                {
+                    Console.WriteLine(name + " is not eligible for admission to an engineering course due to insufficient marks.");
+                }
+            }
+            else
+            {
+                Console.WriteLine(name + " is not eligible for admission to an engineering course due to insufficient marks.");
+            }
+            Console.Read();
         }
     }
-
+}
